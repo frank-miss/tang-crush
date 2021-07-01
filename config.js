6bc2e3710657a5f26f7bed0a2e867f9856cacfc4
frank-miss <86793625+frank-miss@users.noreply.github.com>
2021-07-01 23:45:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'Bạn à!',
+    introDesc: `Sắp thi rồi , chúng ta lên
+    sống tình cảm với nhau hơn
+    trong những ngày này :3`,
+    btnIntro: '😁😁',
+    title: 'Phải chăng kì đợt tới bạn sẽ giúp tôi trong giờ làm 😙',
+    desc: 'Tôi biết bạn  là bạn rất tốt mà :v ',
+    btnYes: 'Giúp chứ , điêu làm chó <33',
+    btnNo: 'Không, Bạn hiền à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao bạn lại tốt với tôi như vậy <3',
+    btnReply: 'Gửi bạn của tôi <3',
+    reply: 'Ta là bạn mà , tôi sẽ giúp hết mình <33333333',
+    mess: 'Tôi biết mà 🥰. Tôi biết trước rồi mà 😘😘',
+    messDesc: 'Hẹn nhau từ 6/7 nhá :3',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
