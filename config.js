6c1ec94f052dd6ea1df5424d1ae58946de63a128
frank-miss <86793625+frank-miss@users.noreply.github.com>
2021-07-01 23:28:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    introTitle: 'Bạn à!',
+    introDesc : Bạn có đang sống thật
+    với giới tính của mình không ? ',
+    btnIntro: '😱No😱',
+    title: 'Ở đây bạn có thể sống thật và chia sẻ thật',
+    desc: 'Bạn có bị Bê đê không ?',
+    btnYes: '😁😁Có😁😁',
+    btnNo: 'Không , Bạn trẻ à :3',
+    question:'Tại sao bạn lại giả dối đến bây giờ ?',
+    btnReply: 'Gửi cho tất cả mọi người',
+    reply: 'Tôi là Bê Đê , bạn đừng nói với ai ',
+    mess: 'Thật ra tôi biết từ lâu rồi :3',
+    messDesc: 'Tôi sẽ giữ bí mật hộ bạn , đừng lo.',
+    btnAccept: 'Cảm ơn bạn <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
